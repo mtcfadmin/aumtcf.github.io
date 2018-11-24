0ac9b2eab6dd3ffd7409f896f44ff2835564fead
sthammadi <sandeep.thammadi@outlook.com>
2018-11-24 13:12:12 +1100

    Christmas update

--- assets/js/mtcf.js
+++ assets/js/mtcf.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    $(document).snowfall({flakeCount : 800, maxSpeed : 2, maxSize : 5});
+    //$(document).snowfall({flakeCount : 800, maxSpeed : 2, maxSize : 5});
 });
