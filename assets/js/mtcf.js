0aecb3f7e03aeeb3eaade3fbb6986cb7cd215b8b
sthammadi <sandeep.thammadi@outlook.com>
2019-02-14 13:27:35 +1100

    March camp updates

--- assets/js/mtcf.js
+++ assets/js/mtcf.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     //$(document).snowfall({flakeCount : 800, maxSpeed : 2, maxSize : 5});
+
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
 });
